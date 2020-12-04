d19a4051d8650a5e74b7f78a4d4e8e1855a6533f
Matthew <bakebill20@yahoo.com>
2020-12-04 13:31:13 -0600

    started string bonus two

---
+++ src/StringBonus2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class StringBonus2 {
+
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int v = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.printf("%d vowels and %d consonants", v, c);
+
+    }
+}
