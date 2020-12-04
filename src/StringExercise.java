0f878b25852f14504186c95d767125a3d70f42e9
Matthew <bakebill20@yahoo.com>
2020-12-04 11:12:15 -0600

    finished bob

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,9 +1,8 @@
 public class StringExercise {
     public static void main (String[] args){
-        String one = "We don't need no education";
+        String one = "We don't need no education" + "\n" +
+        "We don't need no thought control";
         System.out.println(one);
-        String two = "We don't need no thought control";
-        System.out.println(two);
 
         String three = "Check \"this\" out!, \"s inside of \"s!";
         System.out.println(three);
