59b8dff03f89c1e0e249a912c21f7ddbc6fd3ee7
Matthew <matthew.d.baker20@gmail.com>
2020-12-10 19:03:59 -0600

    working on adding a new movie, added the option to do

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,6 +1,5 @@
 package movies;
 import util.Input;
-
 import java.util.Arrays;
 
 public class MoviesApplication {
@@ -16,17 +15,16 @@ public class MoviesApplication {
 
         Movie[] allMovies = MoviesArray.findAll();
 
-
         do{
-            System.out.printf("0 - exit %n1 - view all movies %n" +
+            System.out.printf("0 - exit %n" +
+                    "1 - view all movies %n" +
                     "2 - view movies in the animated category%n" +
                     "3 - view movies in the drama category%n" +
                     "4 - view movies in the horror category%n" +
-                    "5 - view movies in the scifi category");
+                    "5 - view movies in the scifi category%n" +
+                    "6 -- add a new movie%n");
 
             int indexChoice = movieInput.getInt();
-            String filter = genres[indexChoice];
-
 
             if (indexChoice == 0) {
                 keepSearching = false;
@@ -35,12 +33,21 @@ public class MoviesApplication {
                     System.out.print(allMovies[i].getName() + " -- ");
                     System.out.println(allMovies[i].getCategory());
                 }
-                System.out.println("You are viewing movies in the : " + filter + " category");
+                System.out.println("You are viewing all movies");
                 System.out.println(" ");
+            } else if(indexChoice == 6){
+                System.out.println("Enter The Movie Title");
+                String addMovieTitle = movieInput.getString();
+                System.out.println("Enter The Movie Genre");
+                String addMovieCategory = movieInput.getString();
+                Movie addedMovie = new Movie(addMovieTitle, addMovieCategory);
+                Movie[] movieArrWithAddedMovie = Arrays.copyOf(allMovies, allMovies.length + 1);
+                Arrays.fill(movieArrWithAddedMovie, movieArrWithAddedMovie.length - 1, movieArrWithAddedMovie.length, addedMovie);
+                System.out.println(" ");
+            } else if(indexChoice > 1 && indexChoice < 6) {
+                returnList(indexChoice, genres, allMovies);
             }
-            returnList(indexChoice, genres, allMovies);
         }while(keepSearching);
-
     }
 
     public static void returnList(int index, String[] genreList, Movie[] movieList){
@@ -49,9 +56,9 @@ public class MoviesApplication {
             if(movieList[i].getCategory().equalsIgnoreCase(genreChoice)){
                 System.out.print(movieList[i].getName() + " -- ");
                 System.out.println(movieList[i].getCategory());
-                System.out.println("You are viewing movies in the : " + genreChoice + " category");
-                System.out.println(" ");
             }
         }
+        System.out.println("You are viewing movies in the : " + genreChoice + " category");
+        System.out.println(" ");
     }
 }
