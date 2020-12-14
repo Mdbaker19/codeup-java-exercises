d78ebcf76a4cc878da13f6edb40678d990f2249d
Matthew <matthew.d.baker20@gmail.com>
2020-12-14 12:31:04 -0600

    finished exercise

---
+++ src/shapes/Square.java
@@ -0,0 +1,27 @@
+package shapes;
+
+public class Square extends Rectangle{
+
+    public int side;
+
+    public Square (int s){
+        super(s, s);
+    }
+
+    @Override
+    public int getArea(){
+        System.out.println("This is running right? getArea square");
+        return this.width * this.length;
+//        return this.side * this.side;----- THIS DID NOT WORK, NEED TO REFERENCE IT LIKE IT IS REFERENCED IN THE PARENT
+        // RECTANGLE CLASS AS LENGTH AND WIDTH???
+    }
+
+    @Override
+    public int getPerimeter(){
+        System.out.println("This is running right? getPerimeter square");
+        return this.length * 4;
+        //        return this.side * 4;----- THIS DID NOT WORK, NEED TO REFERENCE IT LIKE IT IS REFERENCED IN THE PARENT
+        // RECTANGLE CLASS AS LENGTH AND WIDTH???
+    }
+
+}
