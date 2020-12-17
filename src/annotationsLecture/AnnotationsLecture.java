53a91c96372cdb43c52026accfec14f883e91600
Matthew <matthew.d.baker20@gmail.com>
2020-12-17 16:25:54 -0600

    annotations lecture

--- src/annotationsLecture/AnnotationsLecture.java
+++ src/annotationsLecture/AnnotationsLecture.java
@@ -14,14 +14,14 @@ public class AnnotationsLecture {
 
         //TODO TOGETHER: Call deprecated method & no comment
         //TODO TOGETHER: Call deprecated method with comment
-//        child1.sayHello();
-
+//        child1.deprecatedMethod();
+        // will still run
 
 
 
         //TODO TOGETHER:Call sayHello without @Override
-        //TODO TOGETHER: Comment out annotation and change Parent sayHello no sayHelloParent (Notice there is no error!)
-//        child1.deprecatedMethod();
+        //TODO TOGETHER: Comment out annotation and change Parent sayHello no sayHelloParent (Notice there is no error!) error in console that method is not an override
+        child1.sayHello();
 
 
 //        doRiskyThings();
@@ -39,16 +39,17 @@ public class AnnotationsLecture {
         Child riskyChild = new Child();
         riskyChild.oldMethod(); //static access & deprecation
 
+
         List l = new ArrayList(); // raw use warning
         l.add("test"); // unchecked warning
         int three = 3; // unused warning
+
 
         System.out.println(l);
         AnnotationsLecture al = new AnnotationsLecture();
         System.out.println(al.someNum); // static-access warning
 
         String someName = "Fred"; // unused
-
 
         // Notice these warnings are still present!
         if (true) {
@@ -60,5 +61,31 @@ public class AnnotationsLecture {
             System.out.println("TRUE!");
         }
     }
+
+
+    /*
+    Different Annotations from lecture:
+    ________A form of  metadata__________====== compile time and run time, do not modify existing code
+
+    @ tells compiler "this" is an annotation
+
+    Annotations create a link @Entity(table = "vehicles",  key = "id") linking
+
+    Used in classes (at the top) interfaces, fields and local variables
+
+    Annotation is set above the class, property, method , parameter, or variable
+
+    @Entity
+    @Table(name = "users")
+    @Id
+    @GeneratedValue
+    @Column(nullable = false, unique = true)
+     @Controller
+     @GetMapping("/add/{a}/and{b}")
+     @ResponseBody
+
+
+     */
+
 
 }
--- src/annotationsLecture/Child.java
+++ src/annotationsLecture/Child.java
@@ -9,6 +9,11 @@ public class Child extends Parent {
     /**
      * @deprecated Use newMethod instead {@link #imAMethod()}
      */
+    // this links to a different method, IDE will say to use a deprecated method call with w/e may be in the @link annotation in this
+    // case it is the #imAMethod()
+    // the comment needs to be written this way
+
+
     @Deprecated
     public void deprecatedMethod(){
         System.out.println("I'm a deprecated method!");
